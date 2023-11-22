8e03d20bc0edaf45f985236ff070bf0de8a8df72
Gorkov <ijrjkfl>
2023-11-22 16:15:48 +0300

    Наброски формы

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,64 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Navbar/>
+    <el-table
+        :data="tableData"
+        style="width: 100%">
+      <el-table-column
+          prop="id"
+          label="Идентификатор"
+          width="180">
+      </el-table-column>
+      <el-table-column
+          prop="name"
+          label="Название"
+          width="180">
+      </el-table-column>
+      <el-table-column
+          prop="status_id"
+          label="Статус">
+      </el-table-column>
+      <el-table-column
+          prop="created_at"
+          label="Дата создания">
+      </el-table-column>
+      <el-table-column>
+        <el-button>Удалить</el-button>
+      </el-table-column>
+    </el-table>
+    <TableForm/>
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TableForm from "@/components/TableForm.vue";
+import Navbar from "@/components/Navbar.vue";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TableForm,
+    Navbar
+  },
+  data() {
+    return {
+      tableData: [
+        {
+          id: 1,
+          name: "Приходная накладная",
+          status_id: 1,
+          created_at: "2023-07-04 11:00:00.207814"
+        },
+        {
+          "id": 2,
+          "name": "Расходная накладная",
+          "status_id": 2,
+          "created_at": "2023-07-04 11:00:00.207814"
+        }
+      ]
+    }
   }
+
+
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <el-header style="background: aqua">
+
+    </el-header>
+  </div>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'Navbar',
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
